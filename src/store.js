0fd60d8bc2f278843baf8656ee422b93b5c0190d
nghiadonhu <nghia9x02@gmail.com>
2023-09-18 20:33:30 +0700

    init project

--- src/store.js
+++ src/store.js
@@ -20,17 +20,33 @@ export default createStore({
       if (checkcart) {
         checkcart.quantity++;
         console.log('ok', state.cartItems);
+        window.confirm("Sản phẩm đã có trong giỏ hàng");
       } else {
         state.cartItems.push({ product, quantity: 1 });
         console.log('oke', state.cartItems);
+        window.confirm("Sản phẩm đã được thêm vào giỏ hàng");
       }
+      
     },
+    
     setCartItems(state, cartItems) {
       state.cartItems = cartItems;
     },
   
     XoaCart(state, productId) {
       state.cartItems = state.cartItems.filter(item => item.product.id !== productId);
+    },
+
+    updateCart(state, luutru) {
+      const { productId, quantity } = luutru;
+      const cartItem = state.cartItems.find(item => item.product.id === productId);
+      if (cartItem) {
+        cartItem.quantity = quantity;
+      }
+    },
+
+    clearCart(state) {
+      state.cartItems = [];
     },
   },
   getters: {
@@ -43,7 +59,13 @@ export default createStore({
     },
   },
   actions: {
-    
+    // updateCart({ commit }, payload) {
+    //   commit('updateCart', payload);
+    // },
+
+    // clearCart({ commit }) {
+    //   commit('clearCart');
+    // },
   },
   plugins: [localStoragePlugin], 
 });
--- src/views/products/nguoidung/IndexCart.vue
+++ src/views/products/nguoidung/IndexCart.vue
@@ -6,6 +6,7 @@
       <!-- Hiển thị giỏ hàng -->
       <div class="carts">
         <h2>Giỏ hàng</h2>
+        <div class="table-container">
         <table>
           <thead>
             <tr>
@@ -19,15 +20,21 @@
           <tbody>
             <tr v-for="(item, index) in cartItems" :key="index">
               <td>{{ item.product.name }}</td>
-              <td>{{ item.quantity }}</td>
+              <td>
+                <input type="number" v-model="item.quantity" @change="updateCart(item.product.id, item.quantity)"
+                  min="1" max="99" />
+              </td>
               <td>{{ item.product.price }} đ</td>
               <td>{{ item.quantity * item.product.price }} đ</td>
               <td><button @click="XoaCart(item.product.id)">Xóa</button></td>
             </tr>
           </tbody>
         </table>
+      </div>
         <p>Tổng cộng: {{ tongtien }} đ</p>
         <p>Tổng số sản phẩm: {{ tongtien }}</p>
+        <button @click="checkxoacart">Xóa tất cả giỏ hàng</button>
+
       </div>
     </div>
     <TheFooter />
@@ -64,10 +71,28 @@ export default {
     },
   },
   methods: {
-    
+
     XoaCart(productId) {
       console.log('ok', productId);
       this.$store.commit('XoaCart', productId);
+    },
+    checkxoacart() {
+      const checkxoa = window.confirm("Bạn có muốn xóa tất cả giỏ hàng không?");
+
+      if (checkxoa) {
+
+        this.clearCart();
+      }
+    },
+    //dispatch
+    clearCart() {
+      this.$store.commit('clearCart');
+    },
+
+    updateCart(productId, newQuantity) {
+      this.$store.commit('updateCart', { productId, quantity: newQuantity });
+
+
     },
   },
 };
